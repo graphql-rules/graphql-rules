5fd3b2e2f6a68c8f6667145ea6b6696e7f27a3ed
nodkz <pavel.chertorogov@gmail.com>
2019-06-06 02:23:25 +0600

    typescript tunning

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
@@ -45,9 +44,5 @@ const Layout = ({ children }) => (
     )}
   />
 );
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default Layout;
