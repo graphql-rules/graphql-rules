44eced15aed78e1e116b59932953d1a217518841
nodkz <pavel.chertorogov@gmail.com>
2019-07-07 00:10:48 +0600

    remove unused plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,34 +59,6 @@ module.exports = {
       },
     },
     {
-      resolve: '@stackbit/gatsby-plugin-menus',
-      options: {
-        // static definition of menu items (optional)
-        // menus: {
-        //   main: // identifier of menu container
-        //     [ // array of contained children menu items
-        //       {
-        //         identifier: 'myId', // identifier for this item (optional)
-        //         title: 'Title for page',
-        //         url: '/page-1/',
-        //         weight: 1
-        //       }
-        //     ]
-        //   ]
-        // },
-        // Gatsby node types from which we extract menus (optional, see "Advanced usage")
-        sourceNodeType: 'MarkdownRemark',
-        // the relative node path where we can find the 'menus' container (optional)
-        sourceDataPath: 'frontmatter',
-        // the relative node path where we can find the page's URL (required)
-        sourceUrlPath: 'frontmatter.path',
-        // custom menu loading function (optional)
-        // menuLoader: customLoaderFunction,
-        // the property to use for injecting to the page context (optional, see "Advanced usage")
-        pageContextProperty: 'menus',
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-142402885-1',
